842e3f2b0efca4f02056eed2861cd5a07734bcdc
shamilovstas <shamilovstas@gmail.com>
2021-11-11 23:20:54 +0200

    Replaced the seconds seekbar with three new -- for hours, minutes and seconds respectively

--- app/src/main/java/com/shamilovstas/particleclock/app/MainActivity.kt
+++ app/src/main/java/com/shamilovstas/particleclock/app/MainActivity.kt
@@ -1,53 +1,62 @@
 package com.shamilovstas.particleclock.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.widget.CheckBox
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.shamilovstas.particleclock.ParticleClock
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
-import java.time.temporal.TemporalUnit
 
 class MainActivity : AppCompatActivity() {
+    private var currentTime = LocalTime.now()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val seekbar = findViewById<SeekBar>(R.id.time_seekbar)
+        val secondsSeekbar = findViewById<SeekBar>(R.id.seconds_seekbar)
+        val minutesSeekbar = findViewById<SeekBar>(R.id.minutes_seekbar)
+        val hoursSeekbar = findViewById<SeekBar>(R.id.hours_seekbar)
+
         val timeLabel = findViewById<TextView>(R.id.time)
         val autoMode = findViewById<CheckBox>(R.id.auto_mode)
         val clock = findViewById<ParticleClock>(R.id.clock)
         val formatter = DateTimeFormatter.ISO_LOCAL_TIME
 
-        autoMode.setOnCheckedChangeListener { _, isChecked -> seekbar.isEnabled = !isChecked }
-        seekbar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if (autoMode.isChecked.not()) {
-                    val time = LocalTime.ofSecondOfDay(progress.toLong())
-                    timeLabel.text = formatter.format(time)
-                    clock.setTime(time)
+         val seekbars = listOf(secondsSeekbar, minutesSeekbar, hoursSeekbar)
+        autoMode.setOnCheckedChangeListener { _, isChecked -> seekbars.forEach { it.isEnabled = !isChecked  }}
+        seekbars.forEach {
+            it.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                    if (autoMode.isChecked.not()) {
+                        val minute = minutesSeekbar.progress
+                        val second = secondsSeekbar.progress
+                        val hour = hoursSeekbar.progress
+                        val time = LocalTime.of(hour, minute, second)
+                        timeLabel.text = formatter.format(time)
+                        clock.setTime(time)
+                    }
                 }
-            }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
 
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-        })
-        var currentTime = LocalTime.now()
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+            })
+        }
         val handler = Handler(Looper.getMainLooper())
         var runnable: Runnable? = null
         runnable = Runnable {
-            Log.d("Clock", currentTime.format(DateTimeFormatter.ISO_LOCAL_TIME))
             if (autoMode.isChecked) {
-                val toSecondOfDay = currentTime.toSecondOfDay()
-                Log.d("SecondOfDay", toSecondOfDay.toString())
-                seekbar.progress = toSecondOfDay
+                val minute = currentTime.minute
+                val hour = currentTime.hour
+                val second = currentTime.second
+                secondsSeekbar.progress = second
+                minutesSeekbar.progress = minute
+                hoursSeekbar.progress = hour
                 clock.setTime(currentTime)
                 timeLabel.text = formatter.format(currentTime)
             }
