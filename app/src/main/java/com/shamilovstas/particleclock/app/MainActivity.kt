fac83b8a66d8df10bd428a1b0350bb303809aa32
shamilovstas <shamilovstas@gmail.com>
2021-11-23 23:44:18 +0200

    Updated sample, replaced seekbars with a time picker dialog

--- app/src/main/java/com/shamilovstas/particleclock/app/MainActivity.kt
+++ app/src/main/java/com/shamilovstas/particleclock/app/MainActivity.kt
@@ -1,70 +1,52 @@
 package com.shamilovstas.particleclock.app
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import android.widget.CheckBox
-import android.widget.SeekBar
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.shamilovstas.particleclock.view.ParticleClock
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
     private var currentTime = LocalTime.now()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val secondsSeekbar = findViewById<SeekBar>(R.id.seconds_seekbar)
-        val minutesSeekbar = findViewById<SeekBar>(R.id.minutes_seekbar)
-        val hoursSeekbar = findViewById<SeekBar>(R.id.hours_seekbar)
+        findViewById<Button>(R.id.set_time).setOnClickListener { showTimePickerDialog() }
 
         val timeLabel = findViewById<TextView>(R.id.time)
-        val autoMode = findViewById<CheckBox>(R.id.auto_mode)
         val clock = findViewById<ParticleClock>(R.id.clock)
         val formatter = DateTimeFormatter.ISO_LOCAL_TIME
 
-         val seekbars = listOf(secondsSeekbar, minutesSeekbar, hoursSeekbar)
-        autoMode.setOnCheckedChangeListener { _, isChecked -> seekbars.forEach { it.isEnabled = !isChecked  }}
-        seekbars.forEach {
-            it.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
-                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                    if (autoMode.isChecked.not()) {
-                        val minute = minutesSeekbar.progress
-                        val second = secondsSeekbar.progress
-                        val hour = hoursSeekbar.progress
-                        val time = LocalTime.of(hour, minute, second)
-                        timeLabel.text = formatter.format(time)
-                        clock.setTime(time)
-                    }
-                }
-
-                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-
-                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-            })
-        }
         val handler = Handler(Looper.getMainLooper())
         var runnable: Runnable? = null
         runnable = Runnable {
-            if (autoMode.isChecked) {
-                val minute = currentTime.minute
-                val hour = currentTime.hour
-                val second = currentTime.second
-                secondsSeekbar.progress = second
-                minutesSeekbar.progress = minute
-                hoursSeekbar.progress = hour
-                Log.d("ParticleClock", currentTime.format(DateTimeFormatter.ISO_LOCAL_TIME))
-                clock.setTime(currentTime)
-                timeLabel.text = formatter.format(currentTime)
-            }
+            Log.d("ParticleClock", currentTime.format(DateTimeFormatter.ISO_LOCAL_TIME))
+            clock.setTime(currentTime)
+            timeLabel.text = formatter.format(currentTime)
             currentTime = currentTime.plus(1, ChronoUnit.SECONDS)
             handler.postDelayed(runnable!!, 1000)
         }
         handler.post(runnable)
+    }
+
+    fun showTimePickerDialog() {
+        TimePickerDialog(
+            this,
+            this,
+            currentTime.hour,
+            currentTime.minute,
+            true
+        ).show()
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        currentTime = currentTime.withMinute(minute).withHour(hourOfDay)
     }
 }
